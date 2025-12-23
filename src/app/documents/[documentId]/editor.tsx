996a5864f088c5d46d7d32411cc503f8a53b62af
Just-Hasan <109896294+Just-Hasan@users.noreply.github.com>
2025-12-24 07:54:15 +0800

    add image extension and image resize

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -5,9 +5,11 @@ import { Table } from '@tiptap/extension-table'
 import { TableRow } from '@tiptap/extension-table-row'
 import { TableHeader } from '@tiptap/extension-table-header'
 import { TableCell } from '@tiptap/extension-table-cell'
-
+import ImageResize from 'tiptap-extension-resize-image'
 import { TaskList } from '@tiptap/extension-task-list'
 import { TaskItem } from '@tiptap/extension-task-item'
+import Image from '@tiptap/extension-image'
+
 import StarterKit from '@tiptap/starter-kit'
 
 export const Editor = () => {
@@ -27,6 +29,8 @@ export const Editor = () => {
       TableRow,
       TableHeader,
       TableCell,
+      Image,
+      ImageResize,
     ],
     content: `<table>
           <tbody>
